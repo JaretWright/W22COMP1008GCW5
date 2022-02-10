0f179ad73d7e6dff2d19059d193e68df49e76227
JaretWright <jaret.wright@georgiancollege.ca>
2022-02-09 22:46:56 -0500

    Created the GroceryItem class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        GroceryItem item1 = new GroceryItem("carrots",4.79, "vegetables");
+        System.out.println(item1);
 
-
+        GroceryItem item2 = new GroceryItem("bread",2.89,"bread");
+        System.out.println(item2);
     }
 }
